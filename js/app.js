5a2d59555cd2cc4fae510f4cafa2fafd1fd366a7
TusharGarg11 <tushar.garg.learningAc@gmail.com>
2023-03-26 22:17:09 +0530

    Duck Hunk Game

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// Logic to Move the Aim Image
+function moveAimImage(e){
+let x,y;
+    if( e.clientX - 35 < 0 || e.clientY - 35 < 0){
+        x=e.clientX;
+        y=e.clientY;
+    }else{
+        x=e.clientX - 35;
+        y=e.clientY - 35;
+    }
+
+    var imageDiv=document.getElementById("aimImage");
+    imageDiv.style.left=x + 'px';
+    imageDiv.style.top=y + 'px';
+}
