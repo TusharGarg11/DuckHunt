378a9bc8d6698c79e55ab8cacfc49b922193a49c
TusharGarg11 <tushar.garg.learningAc@gmail.com>
2023-03-31 21:45:41 +0530

    Add Counter & Score

--- js/app.js
+++ js/app.js
@@ -1,21 +1,33 @@
-// Logic to Move the Aim Image
-function moveAimImage(e){
-let x,y;
-    if( e.clientX - 35 < 0 || e.clientY - 35 < 0){
-        x=e.clientX;
-        y=e.clientY;
+let count=0;
+let Timecounter=30;
+// // Logic to Move the Aim Image
+// function moveAimImage(e){
+// let x,y;
+//     if( e.clientX - 35 < 0 || e.clientY - 35 < 0){
+//         x=e.clientX;
+//         y=e.clientY;
+//     }else{
+//         x=e.clientX - 35;
+//         y=e.clientY - 35;
+//     }
+
+//     var imageDiv=document.getElementById("aimImage");
+//     imageDiv.style.left=x + 'px';
+//     imageDiv.style.top=y + 'px';
+// }
+
+setInterval(()=>{
+    if(Timecounter>=0){
+        document.getElementById("currentTimer").innerHTML= --Timecounter;
     }else{
-        x=e.clientX - 35;
-        y=e.clientY - 35;
+        alert("Total Score is :" + count +"\n Reload the game to play again");
     }
-
-    var imageDiv=document.getElementById("aimImage");
-    imageDiv.style.left=x + 'px';
-    imageDiv.style.top=y + 'px';
-}
-
+    
+},1000);
 
 function hideBird(clicked_id){
+    document.getElementById("currentScore").innerHTML= ++count;
+    document.getElementById("fireSound").play();
     document.getElementById(clicked_id).style.display="none";
     let x = Math.floor((Math.random() * 10) + 1)
     setTimeout(()=>{
